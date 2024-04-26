d6c1b1a9b4ebb7a07ac330100a3aa90bda1cf5c6
Arthur <arthur.avagyan@altacode.com>
2024-04-26 20:31:29 +0400

    restructure project

---
+++ Infinite Gallery/Domain/Use Cases/AlbumUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  AlbumUseCase.swift
+//  Infinite Gallery
+//
+//  Created by Arthur Avagyan on 26.04.24.
+//
+
+import Foundation
+
+final class AlbumUseCase {
+	
+}
---
+++ Infinite Gallery/Domain/Use Cases/GalleryUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  GalleryUseCase.swift
+//  Infinite Gallery
+//
+//  Created by Arthur Avagyan on 26.04.24.
+//
+
+import Foundation
+
+final class GalleryUseCase {
+	
+}
